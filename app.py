f48be298804071d3737b15150ab45eb801c0b0f0
ismahan2022 <99269791+ismahan2022@users.noreply.github.com>
2023-03-07 14:36:43 +0000

    Added the database stuff

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, redirect, render_template 
+import psycopg2 
+
+app = Flask(__name__) 
+# conn = psycopg2.connect( host="localhost", database="mydatabase", user="postgres", password="secretpassword" ) 
+
+@app.route('/') 
+def home(): 
+    return render_template('home.html') 
+    
+@app.route('/login', methods=['GET', 'POST']) 
+def login(): 
+    if request.method == 'POST': 
+        username = request.form['username'] 
+        password = request.form['password'] 
+
+        
+        cursor = conn.cursor() 
+        cursor.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'") 
+        user = cursor.fetchone() 
+        if user: 
+            return redirect('/') 
+        else: 
+            return render_template('login.html', message="Login Failed")
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST']) 
+def signup(): 
+    if request.method == 'POST': 
+        username = request.form['username'] 
+        password = request.form['password'] 
+
+        
+        cursor = conn.cursor() 
+        cursor.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'") 
+        user = cursor.fetchone() 
+        if user: 
+            return redirect('/login') 
+        else: 
+            return render_template('signup.html', message="Login Failed")
+    return render_template('signup.html')
+@app.route('/welcome')
+def welcome():
+    return 'Welcome!'
+
+
+if __name__ == '__main__':
+    app.run()
